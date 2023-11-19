44eda3690f781cfd2f77ac30ad8e7dc8a56d787e
adparab97 <adity@Adityas-MacBook-Air.local>
2023-11-18 19:52:38 -0800

    merged with ks

--- old/app.js
+++ old/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
